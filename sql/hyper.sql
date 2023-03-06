5c8cab58f03b453a53a20b5104a4ee83f274ae10
Zoltan Haindrich <zoltan@timescale.com>
2023-03-06 11:01:43 +0000

    a

--- sql/hyper.sql
+++ sql/hyper.sql
@@ -1,11 +1,12 @@
 
-select  :'current_mode' = 'normal' as t_normal,
-        :'current_mode' = 'hyper' as t_hyper,
-        :'current_mode' = 'compressed' as t_compressed
-        \gset
+create or replace function is_hypertable(t_schema text, t_name text)  returns boolean 
+language sql as 
+$BODY$
+select (select true from  timescaledb_information.hypertables where hypertable_schema =t_schema and hypertable_name=t_name) is true;
+$BODY$;
 
-\if :t_normal
-    -- no-op
-\else
+select :'current_mode' != 'normal' AND NOT is_hypertable(:'current_mode',:'table_name') as proceed \gset
+
+\if :proceed
     SELECT create_hypertable(:'table_name', 'time', chunk_time_interval => interval '12 hour', migrate_data=>true);
 \endif
