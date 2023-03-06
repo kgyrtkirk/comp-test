9d748181ecbd4eafb61da1c6ae551b70b2cab1ba
Zoltan Haindrich <zoltan@timescale.com>
2023-03-06 14:01:04 +0000

    add/up/etc

--- sql/test1_int.sql
+++ sql/test1_int.sql
@@ -47,23 +47,7 @@ select  :'current_mode' = 'normal' as t_normal,
 \i append.sql
 \i uncompress.sql
 \i compress.sql
--- \i delete.sql
-\i rename_column.sql
-\i rename_column.sql
-\i rename_column.sql
-\i rename_column.sql
-\i rename_column.sql
-\i unhyper.sql
-\i append.sql
-\i hyper.sql
-\i append.sql
-\i add_column_nullable.sql
-\i add_column_default.sql
-\i column_drop.sql
-\i uncompress.sql
-\i compress.sql
-\i rename_column.sql
-\i append.sql
+\i delete.sql
 -- \i uncompress.sql
 
 \i cmp.sql
