50c8a3544ec81d6def39bfc83143ec38bce23ecf
Zoltan Haindrich <zoltan@timescale.com>
2023-03-06 14:08:29 +0000

    x

--- sql/test1.sql
+++ sql/test1.sql
@@ -13,12 +13,11 @@ show search_path;
 
 \i devices_1.sql
 
--- \set current_mode normal
--- \i test1_int.sql
--- \set current_mode hyper
--- \i test1_int.sql
+\set current_mode normal
+\i test1_int.sql
+\set current_mode hyper
+\i test1_int.sql
 \set current_mode compressed
-\set last_mode compressed
 \i test1_int.sql
 
 \! banner ok
