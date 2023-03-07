7508abf885b803bf8eb067f1cfe9f8af836bc281
Zoltan Haindrich <zoltan@timescale.com>
2023-03-07 12:24:26 +0000

    cleanup

--- sql/diff_mismatch.sql
+++ sql/diff_mismatch.sql
@@ -2,22 +2,14 @@
 \i append.sql
 \i uncompress.sql
 \i compress.sql
--- \i delete.sql
 \i append.sql
 \i rename_column.sql
 \i rename_column.sql
 \i rename_column.sql
 \i rename_column.sql
 \i rename_column.sql
--- \i unhyper.sql
 \i append.sql
--- \i hyper.sql
--- \i append.sql
 \i add_column_nullable.sql
 \i add_column_default.sql
--- \i column_drop.sql
 \i uncompress.sql
 \i compress.sql
--- \i rename_column.sql
--- \i append.sql
--- \i uncompress.sql
--- sql/segfault.sql
+++ sql/segfault.sql
@@ -3,21 +3,15 @@
 \i uncompress.sql
 \i compress.sql
 \i append.sql
--- \i delete.sql
 \i rename_column.sql
 \i rename_column.sql
 \i rename_column.sql
 \i rename_column.sql
 \i rename_column.sql
--- \i unhyper.sql
 \i append.sql
--- \i hyper.sql
--- \i append.sql
 \i add_column_nullable.sql
 \i add_column_default.sql
 \i column_drop.sql
 \i uncompress.sql
 \i compress.sql
--- \i rename_column.sql
 \i append.sql
--- \i uncompress.sql
