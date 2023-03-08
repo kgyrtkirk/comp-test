660a7d4a199aec1da7abddf517531e8e8b61c6e2
Zoltan Haindrich <zoltan@timescale.com>
2023-03-08 13:12:58 +0000

    add segfault-14

---
+++ test/segfault-pg14.sql
@@ -0,0 +1,16 @@
+-- \echo :step0
+-- \set step :step0
+\set step 331
+\i steps/hyper.sql
+-- \set step 7
+\i steps/column_rename.sql
+\i steps/column_rename.sql
+\i steps/column_rename.sql
+\i steps/append.sql
+\i steps/column_add_nullable.sql
+\i steps/column_add_default.sql
+\i steps/column_drop.sql
+\i steps/uncompress.sql
+-- \set step 12
+\i steps/compress.sql
+\i steps/append.sql
