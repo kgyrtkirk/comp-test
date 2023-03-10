ff9361b778bcae5976524177a59358c6afd32421
Zoltan Haindrich <zoltan@timescale.com>
2023-03-10 08:35:44 +0000

    add pgsql

--- runner/test_runner.sql
+++ runner/test_runner.sql
@@ -1,21 +1,21 @@
 \set ON_ERROR_STOP 1
 \set ON_ERROR_ROLLBACK 1
 
-create or replace function load_timescaledb() returns void 
-as
-$$
-declare
-    installed integer;
-begin
-    select count(1) into installed from pg_extension where extname='timescaledb';
-    set search_path=public;
-    if installed < 1 then
-        create extension timescaledb;
-    end if;
-end;
-$$ language plpgsql;
+-- create or replace function load_timescaledb() returns void 
+-- as
+-- $$
+-- declare
+--     installed integer;
+-- begin
+--     select count(1) into installed from pg_extension where extname='timescaledb';
+--     set search_path=public;
+--     if installed < 1 then
+--         create extension timescaledb;
+--     end if;
+-- end;
+-- $$ language plpgsql;
 
-select load_timescaledb();
+-- select load_timescaledb();
 
 \ir information_schema_ext.sql
 
---
+++ test/rnd2.sql
@@ -0,0 +1,16 @@
+-- \i steps/hyper.sql
+-- \i steps/compress.sql
+
+\i steps/hyper.sql
+\set step 7
+\i steps/column_rename.sql
+\i steps/column_rename.sql
+\i steps/column_rename.sql
+\i steps/append.sql
+\i steps/column_add_nullable.sql
+\i steps/column_add_default.sql
+\i steps/column_drop.sql
+\i steps/uncompress.sql
+\set step 12
+\i steps/compress.sql
+\i steps/append.sql
