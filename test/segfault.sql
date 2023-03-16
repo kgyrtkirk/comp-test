c559244bc30dc99bd1727fd6dd108c592b7d5782
Zoltan Haindrich <zoltan@timescale.com>
2023-03-16 16:04:19 +0000

    narrow

--- test/segfault.sql
+++ test/segfault.sql
@@ -1,9 +1,5 @@
 \i steps/hyper.sql
 \i steps/compress.sql
-\set step 7
-\i steps/rename_column.sql
-\set step 10
-\i steps/column_drop.sql
-\set step 13
+alter table :current_mode.readings drop column battery_status;
 explain select count(1) over (partition by time,c),* from  :current_mode.readings c;
 select count(1) over (partition by time,c),* from  :current_mode.readings c;
