80c8b1063d8aa346904567b941dcdd0b75090f0c
Zoltan Haindrich <zoltan@timescale.com>
2023-03-16 15:55:40 +0000

    narrow

--- test/segfault.sql
+++ test/segfault.sql
@@ -8,11 +8,13 @@
 \i steps/rename_column.sql
 \i steps/rename_column.sql
 \i steps/rename_column.sql
-\i steps/append.sql
+\set step 10
+-- \i steps/append.sql
 \i steps/add_column_nullable.sql
 \i steps/add_column_default.sql
 \i steps/column_drop.sql
 -- \i steps/uncompress.sql
+\set step 13
 \i steps/compress.sql
 -- \i steps/append.sql
 explain select count(1) over (partition by time,c),* from  :current_mode.readings c;
